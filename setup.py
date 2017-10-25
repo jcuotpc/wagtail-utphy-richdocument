9ab014b7bc0043bb1fac36807320bd00afdbe22b
Julian Comanean <icomanean@gmail.com>
2017-10-25 14:56:34 -0400

    version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='wagtail-utphy-richdocument',
-    version='0.7.5',
+    version='0.7.6',
     url='https://github.com/jcuotpc/wagtail-utphy-richdocument',
     packages=find_packages(),
     include_package_data=True,
@@ -26,10 +26,7 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
